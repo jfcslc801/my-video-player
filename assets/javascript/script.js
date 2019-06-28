8f8ae1ef73cc03ddd5cfbda0f83cfe63fbd65994
jfrancoc <jfcslc@outlook.com>
2019-06-27 22:06:31 -0600

    Play / Pause Toggle

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,9 @@ const progressBar = document.querySelector('.progress__filled');
 const toggle = document.querySelector('.toggle');
 const skipButton = document.querySelector('[data-skip]');
 const ranges = document.querySelector('.player__slider');
+
+// Play / Pause Toggle 
+function togglePlay(){
+    const method = video.paused ? 'play' : 'pause';
+    video[method]()
+}
