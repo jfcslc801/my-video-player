1a07ed729503fd6a6bc2bffcc268ba350ce2840c
jfrancoc <jfcslc@outlook.com>
2019-06-27 21:59:18 -0600

    Document get element by ID

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+// Document get element by ID
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButton = document.querySelector('[data-skip]');
+const ranges = document.querySelector('.player__slider');
